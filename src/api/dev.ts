dd749eb53a23b6a408f0ecef364119bbe1a402a1
Jieyi Hu <bgjehu@me.com>
2020-04-27 13:01:10 -0500

    respect process.env.NODE_ENV

--- src/api/dev.ts
+++ src/api/dev.ts
@@ -125,7 +125,7 @@ class Watcher extends EventEmitter {
 			throw error;
 		}
 
-		process.env.NODE_ENV = 'development';
+		process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 		process.on('exit', () => {
 			this.close();
